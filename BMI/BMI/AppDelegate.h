39a0c1d9d4ee0cf853a49b02e468691ccaf23457
KHALID ALAHMARI <KHALID@KHALIDs-MacBook-Pro.local>
2013-09-25 02:04:53 -0700

    initial commet

---
+++ BMI/BMI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BMI
+//
+//  Created by KHALID ALAHMARI on 9/25/13.
+//  Copyright (c) 2013 Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BMI/BMI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BMI
+//
+//  Created by KHALID ALAHMARI on 9/25/13.
+//  Copyright (c) 2013 Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
